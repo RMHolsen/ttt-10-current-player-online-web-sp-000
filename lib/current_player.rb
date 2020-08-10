799d6146b36fcea5a6fe14d94558da14e2c53f8b
Rosario Holsen <rholsen@gmail.com>
2020-08-10 20:17:58 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -7,3 +7,10 @@ def turn_count(board)
   end
   return counter
 end 
+
+def current_player(board)
+  if turn_count(board) % 2 == 1 
+    return "X"
+  elsif turn_count(board) % 2 == 0 
+    return "O"
+end 
